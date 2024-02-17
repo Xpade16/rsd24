7c1669cd74858f05ea6099d7f1346f9111aadf71
WaiYanMin <waiyanmin.16@gmail.com>
2024-02-17 16:23:21 +0630

    UI APP Bar

--- x-app/src/App.jsx
+++ x-app/src/App.jsx
@@ -1,15 +1,14 @@
 import AppDrawer from "./components/AppDrawer";
+import Header from "./components/Header";
 import { useUIState } from "./providers/UIStateProvider";
+import { CssBaseline } from "@mui/material";
 
 export default function App() {
 
   const { openDrawer, setOpenDrawer } = useUIState();
-  return <div>
+  return <>
     <AppDrawer />
-    <button onClick={() => {
-      setOpenDrawer(!openDrawer)
-    }}>
-      Toggle
-    </button>
-  </div>
+    <Header/>
+    <CssBaseline/>
+  </>
 }
